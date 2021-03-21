a23d46ec346b877690a499e3d1ed7bc79fb63928
Samuel Moura <fsamuelcarlos@gmail.com>
2021-03-21 18:01:30 -0300

    21_03_2021

--- Exercicio/GO_Exercecio/classe9.go
+++ Exercicio/GO_Exercecio/classe9.go
@@ -8,5 +8,5 @@ var y = 32
 
 func main() {
 	fmt.Printf("Em hexadecimal com 0x %#X, em binario %b, e em hexadecimal %X\n", y, y, y)
-	fmt.Printf("%#X\t%b\t%x\n", y, y, y)
+	fmt.Printf("o valor de y é %v\t, já os demais são: %#X\t%b\t%x\n", y, y, y, y)
 }
