587f619bbe61fba9b0aa1bad39a2975ea27781f1
Marcelo Rocha <marcelo@marcelocavalcante.net>
2021-03-22 19:27:35 -0400

    Exercícios 03, 04 e 05 adicionados

---
+++ Exercicio/ex03.go
@@ -0,0 +1,11 @@
+// 1. Utilizando o operador para declaração curta (ou short) de variável,
+// ATRIBUA os seguintes VALORES para os IDENTIFICADORES "x", "y" and "z":
+
+// a. 42
+// b. "James Bond"
+// c. true
+
+// 2. Agora imprima os valores armazenados nestas variáveis
+// utilizando:
+// a. Uma única chamada de Print (Um único Print)
+// b. Múltiplas chamadas de Print
---
+++ Exercicio/ex04.go
@@ -0,0 +1,12 @@
+// 1. Utilize var para DECLARAR três variáveis. As variáveis
+// devem possuir escopo de pacote. Não atribua VALORES a essas
+// variáveis. Utilize os seguintes IDENTIFICADORES para as
+// variáveis e certifique-se de que as variáveis serão dos tipos:
+// a. identificador "x" do tipo int
+// b. identificador "y" do tipo string
+// c. identificador "z" do tipo bool
+//
+// 2. Na função main:
+// a. Imprima os valores de cada variável
+// b. O compilador atribuiu um valor para cada variável. Como esse
+// valor se chama?
