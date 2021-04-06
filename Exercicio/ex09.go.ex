47a4efa042d69943c3615e3e517e5cb2dcb0f43d
Marcelo Rocha <marcelo@marcelocavalcante.net>
2021-04-05 20:58:44 -0400

    Adicionando exercícios desta semana - 2021-04-05

---
+++ Exercicio/ex09.go.ex
@@ -0,0 +1,30 @@
+// Você possui uma turma com 4 alunos cujos nomes são Pedro, Ricardo, Paula e Marcia.
+// Cada um fez 3 exames e tiveram notas diversas, porém a média final de cada deverá ser igual ou maior a 7.0 para que sejam aprovados.
+// As notas de cada um deles são as seguintes:
+// - Pedro: 4.5, 7.9 e 8.1
+// - Ricardo: 5.7, 6.0 e 9.1
+// - Paula: 6.8, 7.6 e 8.7
+// - Marcia: 7.5, 5.9 e 7.6
+
+// a. Crie uma variável que receba o nome de cada aluno. Estas variáveis devem possuir escopo geral.
+
+// b. Para cada nota de cada aluno, crie uma variável. Estas variáveis de nota precisam ser com escopo de função.
+
+// c. Em sua função main imprima:
+// c.1- "Os alunos da turma são:"
+// c.2- Na linha seguinte imprima o valor das variáveis com os nomes dos 4 alunos.
+// c.3- Chame uma nova função chamada media
+
+// d. a função media deverá imprimir:
+// d.1- A média final de cada aluno indicando o nome do mesmo;
+// d.2- Um resultado booleano que represente "true" para cada aluno que foi aprovado (cuja média seja igual ou maior a 7.0)
+
+// e. Crie uma nova função chamada diplomas
+// e.1- A função main deverá chamar também a função diplomas.
+// e.2- A função diplomas irá imprimir a seguinte linha para cada alunoq ue foi aprovado:
+// "Parabéns pela sua aprovação. Seu diploma estará disponível na próxima semana <NOME>."
+
+// f. Crie uma função chamada reprovados.
+// f.1- A função main também deverá chamar a função reprovados
+// f.2- A função reprovados deverá imprimir o seguinte para cada aluno que foi reprovado:
+// "Não sabe, não sabe, vai ter que aprender.. orelha, de burro, cabeça de et. Brincadeirinha, mas melhor sorte na próxima <ALUNO>."
