477db7fab3e3cdd568a8cd427e8b80a50a095803
Samuel Moura <fsamuelcarlos@gmail.com>
2021-04-09 19:15:36 -0300

    go

---
+++ Exercicio/GO_Exercecio/ex01.go
@@ -0,0 +1,31 @@
+// Utilize o código a seguir para as 3 seguintes perguntas:
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	soma := 15 + 12
+
+	fmt.Println("O resultado de soma é:", soma)
+}
+
+// 1- Quem é a variável?
+// a) ( ) 15 + 12
+// b) ( ) fmt
+// c) (X) soma
+// d) ( ) main
+//
+// 2- Qual dos seguintes seria um operando?
+// a) ( ) +
+// b) (X) 12
+// c) ( ) soma
+// d) ( ) main
+//
+// 3- Qual dos seguintes seria um operador?
+// a) (X) +
+// b) ( ) 12
+// c) ( ) soma
+// d) ( ) main
